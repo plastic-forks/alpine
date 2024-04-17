00d71ce6d9c1ef9a7b5f4f6a1505419dfa9570de
c4710n <c4710n@users.noreply.github.com>
2024-04-17 13:50:04 +0800

    feat: remove packages/ui

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,8 @@
-let { writeToPackageDotJson, getFromPackageDotJson } = require('./utils');
-let fs = require('fs');
-let zlib = require('zlib');
+let { writeToPackageDotJson, getFromPackageDotJson } = require('./utils')
+let fs = require('fs')
+let zlib = require('zlib')
 
-([
+;[
     // Packages:
     'alpinejs',
     'csp',
@@ -16,22 +16,21 @@ let zlib = require('zlib');
     'focus',
     'sort',
     'mask',
-    'ui',
-]).forEach(package => {
-    if (! fs.existsSync(`./packages/${package}/dist`)) {
-        fs.mkdirSync(`./packages/${package}/dist`, 0744);
+].forEach((package) => {
+    if (!fs.existsSync(`./packages/${package}/dist`)) {
+        fs.mkdirSync(`./packages/${package}/dist`, 0744)
     }
 
     // Go through each file in the package's "build" directory
     // and use the appropriate bundling strategy based on its name.
-    fs.readdirSync(`./packages/${package}/builds`).forEach(file => {
+    fs.readdirSync(`./packages/${package}/builds`).forEach((file) => {
         bundleFile(package, file)
-    });
+    })
 })
 
 function bundleFile(package, file) {
     // Based on the filename, give esbuild a specific configuration to build.
-    ({
+    ;({
         // This output file is meant to be loaded in a browser's <script> tag.
         'cdn.js': () => {
             build({
@@ -53,7 +52,6 @@ function bundleFile(package, file) {
             }).then(() => {
                 outputSize(package, `packages/${package}/dist/${file.replace('.js', '.min.js')}`)
             })
-
         },
         // This file outputs two files: an esm module and a cjs module.
         // The ESM one is meant for "import" statements (bundlers and new browsers)
@@ -85,20 +83,24 @@ function build(options) {
     options.define || (options.define = {})
 
     options.define['ALPINE_VERSION'] = `'${getFromPackageDotJson('alpinejs', 'version')}'`
-    options.define['process.env.NODE_ENV'] = process.argv.includes('--watch') ? `'production'` : `'development'`
+    options.define['process.env.NODE_ENV'] = process.argv.includes('--watch')
+        ? `'production'`
+        : `'development'`
 
-    return require('esbuild').build({
-        logLevel: process.argv.includes('--watch') ? 'info' : 'warning',
-        watch: process.argv.includes('--watch'),
-        // external: ['alpinejs'],
-        ...options,
-    }).catch(() => process.exit(1))
+    return require('esbuild')
+        .build({
+            logLevel: process.argv.includes('--watch') ? 'info' : 'warning',
+            watch: process.argv.includes('--watch'),
+            // external: ['alpinejs'],
+            ...options,
+        })
+        .catch(() => process.exit(1))
 }
 
 function outputSize(package, file) {
     let size = bytesToSize(zlib.brotliCompressSync(fs.readFileSync(file)).length)
 
-    console.log("\x1b[32m", `${package}: ${size}`)
+    console.log('\x1b[32m', `${package}: ${size}`)
 }
 
 function bytesToSize(bytes) {
@@ -106,5 +108,5 @@ function bytesToSize(bytes) {
     if (bytes === 0) return 'n/a'
     const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
     if (i === 0) return `${bytes} ${sizes[i]}`
-    return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`
+    return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`
 }
